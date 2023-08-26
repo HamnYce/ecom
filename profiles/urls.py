139f054b9ac10b2e063315b63d870acb65c29058
Jant <hamadboa@gmail.com>
2023-08-26 15:22:26 +0300

    Update url syntax formatting

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,6 @@ from .views import (UserProfileListApiView, UserProfileDetailApiView)
 
 urlpatterns = [
     path('all/', UserProfileListApiView.as_view(), name='profile_list'),
-    path('u/<str:username/', UserProfileDetailApiView.as_view(), name='product_detail')
+    path('u/<str:username>/', UserProfileDetailApiView.as_view(),
+         name='product_detail')
 ]
