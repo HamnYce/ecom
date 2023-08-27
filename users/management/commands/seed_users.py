b3c08a31e66ce1616adcf66a6a331cab241782c7
Jant <hamadboa@gmail.com>
2023-08-27 03:41:01 +0300

    remove seeding command in pref. to loaddata

--- users/management/commands/seed_users.py
+++
@@ -1,8 +0,0 @@
-from base_commands.base_seed_command import BaseSeedCommand
-from users.factory import UserFactory
-
-
-class Command(BaseSeedCommand):
-    def __init__(self, *args, **kwargs):
-        self.factory = UserFactory
-        super().__init__(*args, **kwargs)
