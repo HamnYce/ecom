d35e0adefbfdcc213f62684369e50058add9f36d
Jant <hamadboa@gmail.com>
2023-08-28 23:53:56 +0300

    update product model and migrations

--- products/models.py
+++ products/models.py
@@ -1,16 +1,24 @@
-from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils.text import slugify
 
+from profiles.models import UserProfile
 
+# class CategoryStatus(models.Model):
+#     name = models.CharField(max_length=10)
+
+
+# NOTE: a decision to keep the name not a primary key has been made to speed up
+#   results, one way to remedy this when presenting to the users in the front-end
+#   could be to store the list of categories locally on the users device (with sqlite for example)
+#   and hit the local database instead for quick searches
 class Category(models.Model):
     # NOTE: these wil be defined inhouse
     name = models.CharField(max_length=100)
 
 
 class Product(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+    owner = models.ForeignKey(UserProfile, to_field='username',
                               on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=150)
     description = models.TextField()
