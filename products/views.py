e11d4b895a033b1d04250269b3390b925ca842c9
Jant <hamadboa@gmail.com>
2023-08-29 02:02:06 +0300

    add product_list: get,post, category_list: get

--- products/views.py
+++ products/views.py
@@ -1,9 +1,18 @@
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+import rest_framework.status as status
 
 from profiles.permissions import HasProfileOrReadOnly
 
 from .paginators import ProductPaginator
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
+
+# TODO: url product/u/<str:username>/ that routes to users products
+# TODO: decide on list of acceptable filter params
+# TODO: add if statements with filter for queryset then page at the end
 
 
 class ProductListApiView(APIView):
@@ -11,19 +20,37 @@ class ProductListApiView(APIView):
     pagination_class = ProductPaginator()
 
     "returns list of products, will check kwargs for filter"
-    # TODO: decide on list of acceptable filter params
     """
     acceptable filters atm:
-        - username
         - category
         - status_category
+
+        - not username as that can be done through the specified endpoint
     """
 
-    def get(self, request, *args, **kwargs):
-        pass
+    def get(self, request: Request, *args, **kwargs):
+        queryset = Product.objects.all()
+
+        #   filters suupplied in request.queryparams.get()
+
+        page = self.pagination_class.paginate_queryset(
+            queryset=queryset, request=request)
+        if page is not None:
+            serializer = ProductSerializer(page, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
     def post(self, request, *args, **kwargs):
-        pass
+        data = request.POST.dict()
+        serializer = ProductSerializer(data=data)
+        print(request.user)
+
+        if serializer.is_valid():
+            serializer.save(owner=request.user.userprofile)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ProductDetailApiView(APIView):
@@ -34,3 +61,9 @@ class ProductDetailApiView(APIView):
 
     def put(self, request, *args, **kwargs):
         pass
+
+
+class CategoryListApiView(APIView):
+    def get(self, request, *args, **kwargs):
+        serializer = CategorySerializer(Category.objects.all(), many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
