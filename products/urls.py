9dfe297b43109ef092eb104d7ea6376ff83e04a7
Jant <hamadboa@gmail.com>
2023-08-29 02:01:17 +0300

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import ProductDetailApiView, ProductListApiView
+from .views import (ProductDetailApiView,
+                    ProductListApiView, CategoryListApiView)
 
 urlpatterns = [
     path('all/', ProductListApiView.as_view(), name='product_list'),
-    path('u/<str:username>', ProductDetailApiView.as_view(), name='product_detail')
+    path('u/<str:username>/<slug:product_slug>/',
+         ProductDetailApiView.as_view(), name='product_detail'),
+    path('categories/', CategoryListApiView.as_view(), name='category_list')
 ]
