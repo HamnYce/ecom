bd87b5a9f05761f75e8ba254a4cd713fa552b8ce
Jant <hamadboa@gmail.com>
2023-08-28 23:53:15 +0300

    minor bug fix

--- profiles/permissions.py
+++ profiles/permissions.py
@@ -4,6 +4,6 @@ from rest_framework import permissions
 class HasProfileOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
-            request.user.has_profile or
-            request.method in permissions.SAFE_METHODS
+            request.method in permissions.SAFE_METHODS or
+            request.user.has_profile
         )
